edfa0e68231aa5360162ee4365bfaa63f7118369
Yashraj Zope <yashajzope143@gmail.com>
2023-09-11 15:43:50 +0530

    just trained a first model

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+results = model.train(data="config.yaml", epochs=1)  # train the model
